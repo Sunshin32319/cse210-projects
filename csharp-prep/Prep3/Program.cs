ca9e351f819ec522d46938eb03cdece6b43073b7
cjgoodwin <christine_goodwin321@yahoo.com>
2024-09-26 02:05:23 -0500

    initial saving point

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int answer = 1;
+        Console.WriteLine("Guess a number! What is the number? ");
+        string userGuess = Console.ReadLine();
+        int userNumber = int.Parse(userGuess);
+
+        if (answer < userNumber)
+        {
+            Console.WriteLine("Lower!");
+        }
+        else if (answer > userNumber)
+        {
+            Console.WriteLine("Higher!");
+        }
+        else if (answer == userNumber)
+        {
+            Console.WriteLine("You Guessed it! Congrats!");
+        }
+        else
+        {
+            Console.WriteLine("Uh oh! Something's not right. Try again later.");
+        }
     }
 }
