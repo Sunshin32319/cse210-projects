fc0596b8d39b29072ccab6e6d23e15759aae5187
cjgoodwin <christine_goodwin321@yahoo.com>
2024-09-30 13:25:51 -0500

    Prep 4 initial commit

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        // Define User Number and create list
+        // commented out for error o line 19 userNumber;
+        List<int>numbers = new List<int>();
+        int userNumber;
+
+        // print instructions
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        // get user input and start the loop
+        do{
+            Console.Write("Enter number: ");
+            string userInput = Console.ReadLine();
+            userNumber = int.Parse(userInput);
+        } while (userNumber != 0);
     }
 }
