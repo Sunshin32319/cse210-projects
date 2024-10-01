fa4f390a78dccd6030e8b1ce6bef7746bec6230c
cjgoodwin <christine_goodwin321@yahoo.com>
2024-09-30 19:32:31 -0500

    stopping point

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -5,7 +5,11 @@ class Program
 {
     static void Main(string[] args)
     {
+        // first function
         DisplayWelcomeMessage();
+
+        // Declare userName variable and call function
+        string userName = promptUserName();
 
         // DisplayResult();
     }
@@ -13,5 +17,13 @@ class Program
     static void DisplayWelcomeMessage()
     {
         Console.WriteLine("Welcome to the program!");
+    }
+    // PromptUserName function
+    static string promptUserName()
+    {
+        // prompt user name
+        Console.WriteLine("What is your name? ");
+        
+        // write user name
     }
 }
