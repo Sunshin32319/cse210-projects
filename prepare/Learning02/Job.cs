dd03adc3c33c0d7813eab92587acfbdde8a4c5fb
cjgoodwin <christine_goodwin321@yahoo.com>
2024-10-01 15:10:54 -0500

    initial commit

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Job
+{
+    // Instructions step 3.3 'Create member variables in the class for each element 
+    // that this class should contain. By convention these member variables should begin 
+    // with an underscore and a lowercase letter such as _jobTitle .'
+
+    public string _jobTitle;
+    public string _companyName;
+}
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,7 +1,11 @@
 using System;
 
+// step 3 - create the Job class
+
+
 class Program
 {
+    // Step 2 complete - verified the program runs as provided
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
